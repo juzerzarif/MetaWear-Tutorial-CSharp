41d1cc6e90b6a9f172ab494f5d6451067810c428
etsai <etsai2@gmail.com>
2017-07-04 15:00:57 -0700

    Updated free fall detector to use new C# SDK

--- FreeFall Detector/DetectorSetup.xaml.cs
+++ FreeFall Detector/DetectorSetup.xaml.cs
@@ -1,7 +1,4 @@
-﻿using MbientLab.MetaWear.Core;
-using MbientLab.MetaWear.Processor;
-using static MbientLab.MetaWear.Functions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,6 +15,10 @@ using Windows.UI.Xaml.Navigation;
 using System.Runtime.InteropServices;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using MbientLab.MetaWear;
+using MbientLab.MetaWear.Sensor;
+using MbientLab.MetaWear.Builder;
+using MbientLab.MetaWear.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,146 +27,56 @@ namespace FreeFall_Detector {
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class DetectorSetup : Page {
-        private LogDownloadHandler logDlHandler;
-        private Dictionary<String, Fn_IntPtr> dataprocDelegates;
-        private Fn_IntPtr_Int initDelegate;
-        private Fn_IntPtr[] logReadyDelegates;
-        private BluetoothLEDevice selectedDevice;
-        private BtleConnection btleConn;
-        private IntPtr board;
-        private Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic notifyChar;
+        private IMetaWearBoard metawear;
 
         public DetectorSetup() {
-            this.InitializeComponent();
+            InitializeComponent();
 
-            btleConn = new BtleConnection();
-            btleConn.writeGattChar = new Fn_IntPtr_IntPtr_ByteArray(writeCharacteristic);
-            btleConn.readGattChar = new Fn_IntPtr_IntPtr(readCharacteristic);
-
-            initDelegate = new Fn_IntPtr_Int(initialized);
-
-            dataprocDelegates = new Dictionary<string, Fn_IntPtr>();
-
-            logReadyDelegates = new Fn_IntPtr[2];
-        }
-
-        private void initialized(IntPtr board, int status) {
-            System.Diagnostics.Debug.WriteLine("Board Initialized!");
-
-            mbl_mw_metawearboard_set_time_for_response(board, 500);
-            mbl_mw_acc_set_odr(board, 50f);
-            mbl_mw_acc_write_acceleration_config(board);
             
-            var acc_signal = mbl_mw_acc_get_acceleration_data_signal(board);
-
-            dataprocDelegates["ff"] = new Fn_IntPtr(ff => {
-                dataprocDelegates["ff_handler"] = new Fn_IntPtr(dataPtr => {
-                    System.Diagnostics.Debug.WriteLine("In FreeFall");
-                });
-                logReadyDelegates[0] = new Fn_IntPtr(logger => {
-                    mbl_mw_logger_subscribe(logger, dataprocDelegates["ff_handler"]);
-                    System.Diagnostics.Debug.WriteLine("Free Fall logger ready");
-                    System.Diagnostics.Debug.WriteLine("Processor Setup Complete");
-                });
-                mbl_mw_datasignal_log(ff, logReadyDelegates[0]);
-            });
-            dataprocDelegates["no_ff"] = new Fn_IntPtr(noFF => {
-                dataprocDelegates["no_ff_handler"] = new Fn_IntPtr(dataPtr => {
-                    System.Diagnostics.Debug.WriteLine("Not in FreeFall");
-                });
-                logReadyDelegates[1] = new Fn_IntPtr(logger => {
-                    mbl_mw_logger_subscribe(logger, dataprocDelegates["no_ff_handler"]);
-                    System.Diagnostics.Debug.WriteLine("No Free Fall logger ready");
-                });
-                mbl_mw_datasignal_log(noFF, logReadyDelegates[1]);
-            });
-            dataprocDelegates["threshold"] = new Fn_IntPtr(ths => {
-                mbl_mw_dataprocessor_comparator_create(ths, Comparator.Operation.EQ, 1, dataprocDelegates["no_ff"]);
-                mbl_mw_dataprocessor_comparator_create(ths, Comparator.Operation.EQ, -1, dataprocDelegates["ff"]);
-            });
-            dataprocDelegates["avg"] = new Fn_IntPtr(avg => {
-                mbl_mw_dataprocessor_threshold_create(avg, Threshold.Mode.BINARY, 0.5f, 0,
-                    dataprocDelegates["threshold"]);
-            });
-            dataprocDelegates["rss"] = new Fn_IntPtr(rss => {
-                mbl_mw_dataprocessor_average_create(rss, 4, dataprocDelegates["avg"]);
-            });
-            //whoops, wrong fn name
-            mbl_mw_dataprocessor_rss_create(acc_signal, dataprocDelegates["rss"]);
         }
-
-        private async void writeCharacteristic(IntPtr caller, IntPtr charPtr, IntPtr value, byte length) {
-            byte[] managedArray = new byte[length];
-            Marshal.Copy(value, managedArray, 0, length);
-
-            var charGuid = Marshal.PtrToStructure<MbientLab.MetaWear.Core.GattCharacteristic>(charPtr).toGattCharGuid();
-            var status = await selectedDevice.GetGattService(charGuid.serviceGuid).GetCharacteristics(charGuid.guid).FirstOrDefault()
-                .WriteValueAsync(managedArray.AsBuffer(), GattWriteOption.WriteWithoutResponse);
-
-            if (status != GattCommunicationStatus.Success) {
-                System.Diagnostics.Debug.WriteLine("Error writing gatt characteristic");
-            }
-        }
-
-        private async void readCharacteristic(IntPtr caller, IntPtr charPtr) {
-            var charGuid = Marshal.PtrToStructure<MbientLab.MetaWear.Core.GattCharacteristic>(charPtr).toGattCharGuid();
-            var result = await selectedDevice.GetGattService(charGuid.serviceGuid).GetCharacteristics(charGuid.guid).FirstOrDefault()
-                .ReadValueAsync();
-
-            if (result.Status == GattCommunicationStatus.Success) {
-                mbl_mw_connection_char_read(board, charPtr, result.Value.ToArray(), (byte)result.Value.Length);
-            } else {
-                System.Diagnostics.Debug.WriteLine("Error reading gatt characteristic");
-            }
-        }
-
+        
         protected override async void OnNavigatedTo(NavigationEventArgs e) {
             base.OnNavigatedTo(e);
 
-            selectedDevice = e.Parameter as BluetoothLEDevice;
-            notifyChar = selectedDevice.GetGattService(GattCharGuid.METAWEAR_NOTIFY_CHAR.serviceGuid).GetCharacteristics(GattCharGuid.METAWEAR_NOTIFY_CHAR.guid).FirstOrDefault();
-            await notifyChar.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
-            notifyChar.ValueChanged += new TypedEventHandler<Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic, GattValueChangedEventArgs>(
-                (Windows.Devices.Bluetooth.GenericAttributeProfile.GattCharacteristic sender, GattValueChangedEventArgs obj) => {
-                    byte[] response = obj.CharacteristicValue.ToArray();
-                    mbl_mw_connection_notify_char_changed(board, response, (byte)response.Length);
-                });
+            metawear = MbientLab.MetaWear.Win10.Application.GetMetaWearBoard(e.Parameter as BluetoothLEDevice);
+            var accelerometer = metawear.GetModule<IAccelerometer>();
 
-            board = mbl_mw_metawearboard_create(ref btleConn);
-            mbl_mw_metawearboard_initialize(board, initDelegate);
+            accelerometer.Configure(odr: 50f);
+            await accelerometer.Acceleration.AddRouteAsync(source =>
+                source.Map(Function1.Rss).Average(4).Find(Threshold.Binary, 0.5f)
+                    .Multicast()
+                        .To().Filter(Comparison.Eq, -1).Log(data => System.Diagnostics.Debug.WriteLine("In FreeFall"))
+                        .To().Filter(Comparison.Eq, 1).Log(data => System.Diagnostics.Debug.WriteLine("Not in FreeFall"))
+            );
         }
 
-        private void start_Click(object sender, RoutedEventArgs e) {
-            mbl_mw_logging_start(board, 0);
-            mbl_mw_acc_enable_acceleration_sampling(board);
-            mbl_mw_acc_start(board);
+        private void start_Click(object sender, RoutedEventArgs e) { 
+            metawear.GetModule<ILogging>().Start();
+
+            var accelerometer = metawear.GetModule<IAccelerometer>();
+            accelerometer.Acceleration.Start();
+            accelerometer.Start();
         }
 
         private void stop_Click(object sender, RoutedEventArgs e) {
-            mbl_mw_acc_stop(board);
-            mbl_mw_acc_disable_acceleration_sampling(board);
-            mbl_mw_logging_stop(board);
+            metawear.GetModule<ILogging>().Stop();
+
+            var accelerometer = metawear.GetModule<IAccelerometer>();
+            accelerometer.Stop();
+            accelerometer.Acceleration.Stop();
         }
 
-        private void download_Click(object sender, RoutedEventArgs e) {
-            logDlHandler = new LogDownloadHandler();
-            logDlHandler.receivedProgressUpdate = new Fn_Uint_Uint((uint nEntriesLeft, uint totalEntries) => {
-                if (nEntriesLeft == 0) {
-                    System.Diagnostics.Debug.WriteLine("Log Download Completed!");
-                }
-            });
-            logDlHandler.receivedUnknownEntry = new Fn_Ubyte_Long_ByteArray((byte id, long epoch, IntPtr value, byte length) => {
-                System.Diagnostics.Debug.WriteLine("Received unknown log data: id= " + id);
-            });
-
-            mbl_mw_logging_download(board, 0, ref logDlHandler);
+        private async void download_Click(object sender, RoutedEventArgs e) {
+            await metawear.GetModule<ILogging>().DownloadAsync();
+            System.Diagnostics.Debug.WriteLine("Log Download Completed!");
         }
 
         private void back_Click(object sender, RoutedEventArgs e) {
-            mbl_mw_metawearboard_tear_down(board);
-            mbl_mw_metawearboard_free(board);
-
-            this.Frame.Navigate(typeof(MainPage));
+            if (!metawear.InMetaBootMode) {
+                metawear.TearDown();
+                metawear.GetModule<IDebug>().DisconnectAsync();
+            }
+            Frame.GoBack();
         }
     }
 }
--- FreeFall Detector/MainPage.xaml.cs
+++ FreeFall Detector/MainPage.xaml.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using MbientLab.BtleDeviceScanner;
+using MbientLab.MetaWear;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,67 +21,38 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace FreeFall_Detector
-{
-    public sealed class MacAddressHexString : IValueConverter {
-        public object Convert(object value, Type targetType, object parameter, string language) {
-            string hexString = ((ulong)value).ToString("X");
-            return hexString.Insert(2, ":").Insert(5, ":").Insert(8, ":").Insert(11, ":").Insert(14, ":");
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language) {
-            throw new NotImplementedException();
-        }
-    }
-
-    public sealed class ConnectionStateColor : IValueConverter {
-        public SolidColorBrush ConnectedColor { get; set; }
-        public SolidColorBrush DisconnectedColor { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, string language) {
-            switch ((BluetoothConnectionStatus)value) {
-                case BluetoothConnectionStatus.Connected:
-                    return ConnectedColor;
-                case BluetoothConnectionStatus.Disconnected:
-                    return DisconnectedColor;
-                default:
-                    throw new MissingMemberException("Unrecognized connection status: " + value.ToString());
-            }
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language) {
-            throw new NotImplementedException();
-        }
-    }
-
+namespace FreeFall_Detector {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
+    public sealed partial class MainPage : Page, IScanConfig {
+        public MainPage() {
+            InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e) {
+        public int Duration => 10000;
+
+        public Action<BluetoothLEDevice> SelectedDevice => async (item) => {
+            ContentDialog initPopup = new ContentDialog() {
+                Title = "Initializing API",
+                Content = "Please wait while the app initializes the API"
+            };
+
+            initPopup.ShowAsync();
+            var board = MbientLab.MetaWear.Win10.Application.GetMetaWearBoard(item);
+            await board.InitializeAsync();
+            initPopup.Hide();
+
+            Frame.Navigate(typeof(DetectorSetup), item);
+        };
+
+        public List<Guid> ServiceUuids => new List<Guid>(new Guid[] { Constants.METAWEAR_GATT_SERVICE });
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e) {
             base.OnNavigatedTo(e);
 
-            refreshDevices_Click(null, null);
-        }
-
-        private async void refreshDevices_Click(object sender, RoutedEventArgs e) {
-            pairedDevices.Items.Clear();
-            foreach (var info in await DeviceInformation.FindAllAsync(BluetoothLEDevice.GetDeviceSelector())) {
-                pairedDevices.Items.Add(await BluetoothLEDevice.FromIdAsync(info.Id));
-            }
-        }
-
-        private void pairedDevices_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            BluetoothLEDevice selected = ((ListView)sender).SelectedItem as BluetoothLEDevice;
-
-            if (selected != null) {
-                this.Frame.Navigate(typeof(DetectorSetup), selected);
+            if (Frame.BackStack.Count == 0) {
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => Frame.Navigate(typeof(Scanner), this));
             }
         }
     }
